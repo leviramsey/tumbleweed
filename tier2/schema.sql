4309cc7a89fe39406c564a7111878ce0462d15e0
Levi Ramsey <leviable@gmail.com>
2014-04-16 17:24:46 -0400

    Update schema for extended data

--- tier2/schema.sql
+++ tier2/schema.sql
@@ -1,8 +1,10 @@
 -- SQL DB schema for Tumbleweed
 
+CREATE DATABASE IF NOT EXISTS tumbleweed;
+
 USE tumbleweed;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	uid INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(16) NOT NULL,
 	email VARCHAR(32) NOT NULL,
@@ -12,16 +14,27 @@ CREATE TABLE users (
 ) ENGINE 'InnoDB';
 
 --- Not yet implemented in tier2
-CREATE TABLE user_verifies (
+CREATE TABLE IF NOT EXISTS user_verifies (
 	uid INT(8) UNSIGNED PRIMARY KEY,
 	code VARCHAR(8) NOT NULL,
 	FOREIGN KEY (uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE 'InnoDB';
 
 -- Not all users will log in via local passwords (e.g. facebook, google, etc. logins)
-CREATE TABLE user_auths (
+CREATE TABLE IF NOT EXISTS user_auths (
 	uid INT(8) UNSIGNED PRIMARY KEY,
 	hash VARCHAR(31) NOT NULL,
 	cost INT(2) UNSIGNED NOT NULL,
 	FOREIGN KEY (uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE 'InnoDB';
+
+-- The extra attributes for user profile pages
+CREATE TABLE IF NOT EXISTS user_extdata (
+	uedid INT(10) UNSIGNED PRIMARY KEY,
+	uid INT(8) UNSIGNED NOT NULL,
+	k VARCHAR(16) NOT NULL,
+	v TINYTEXT NOT NULL,
+	priority INT(2) UNSIGNED NOT NULL DEFAULT 0,
+	display BOOLEAN NOT NULL DEFAULT 0,
+	FOREIGN KEY (uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE 'InnoDB'
