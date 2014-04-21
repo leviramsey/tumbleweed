51e73da22bb33ff3bcc34702ac0d316459d934b3
Alan Zaffetti <azco21@cox.net>
2014-04-21 04:50:06 -0400

    Re....

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+// TDR ADDED:
+// using connect-flash to provide flash support
+// across redirects:
+var flash = require('connect-flash');
+/////
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 8081);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+// TDR: express.json() and express.urlencoded() are the same
+//      as express.bodyParser() in previous versions of the
+//      connect middleware library.
+app.use(express.json());
+app.use(express.urlencoded());
+/////
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+// TDR ADDED:
+// This is where we add the middleware to the express environment
+// for flash support for redirects:
+app.use(flash());
+/////
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route Definitions:
+var routes = require('./routes');
+app.get('/', routes.index);
+app.get('/feed', routes.feed);
+app.get('/logout', routes.logout);
+app.post('/register', routes.register);
+app.post('/login', routes.login);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,88 @@
+var query = require('../lib/query');
+
+exports.index = function(req, res) {
+
+	if(req.session.user) {
+		res.redirect('/feed');
+	}
+	else {
+		res.render('index', {
+			title: 'Tubmleweed',
+			registererr: req.flash('register'),
+			loginerr: req.flash('login')
+		});
+	}
+}
+
+exports.feed = function(req, res) {
+
+	if(req.session.user) {
+		res.render('feed', {
+			title: req.session.user.name + '\'s Tumblefeed Page',
+			user: req.session.user
+		});
+	}
+	else {
+		res.redirect('/');
+	}
+}
+
+exports.register = function(req, res) {
+
+	if (!req.body.user || !req.body.pass || !req.body.email || !req.body.long_name || !req.body.day || !req.body.month || !req.body.year) {
+    req.flash('register', 'Must fill in all fields');
+    res.redirect('/');
+    return;
+  }
+
+	Query.User.create(req.body.user, req.body.email, req.body.pass, req.body.long_name,
+		{ day: req.body.day, month: req.body.month, year: req.body.year },
+		function(status, user) {
+		
+			if(parseInt(status) > 0) {
+				req.flash('register', 'Username already exists');
+				res.redirect('/');
+			}
+			else {
+				Query.User.info(req.body.user, function(body) {
+					req.session.user = body;
+					res.redirect('/feed');
+				});
+			}
+		});
+};
+
+exports.login = function(req, res) {
+
+	if(!req.body.user || !req.body.pass) {
+		req.flash('login', 'Must give username and password');
+    res.redirect('/');
+    return;
+	}
+
+	Query.User.auth(req.body.user, req.body.pass,
+		function(response, body) {
+		
+			if(parseInt(body.status) > 0) {
+				req.flash('login', 'Username or password invalid');
+				res.redirect('/');
+			}
+			else {
+				Query.User.info(req.body.user, function(body) {
+					req.session.user = body;
+					res.redirect('/feed');
+				});
+			}
+		});
+};
+
+exports.logout = function(req, res) {
+	if(!req.session.user) {
+		res.redirect('/');
+	}
+	else {
+		req.session.destroy(function() {
+			res.redirect('/');
+		});
+	}
+}
