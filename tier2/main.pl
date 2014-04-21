bfc7d3f9b9d4e0387230fc640b847b8f0f31fcbf
Levi Ramsey <leviable@gmail.com>
2014-04-20 20:41:51 -0400

    Moar logging

--- tier2/main.pl
+++ tier2/main.pl
@@ -222,6 +222,7 @@ sub create_user { (my $obj) = @_;
 					unless (defined $uid) {
 						die_error_hash($ret, 5, 'Could not create user');
 					}
+					log_it("info", $@);
 
 					(my $pwhash, my $pwcost)=crypt_password($auth);
 					log_it("info", $@);
